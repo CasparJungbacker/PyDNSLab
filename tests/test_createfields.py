68540ed062649df8f21991f7b51a36da7a08b431
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2022-10-10 21:30:19 +0200

    Add fields + tests

--- tests/test_createfields.py
+++ tests/test_createfields.py
@@ -1,6 +1,12 @@
+import os
 import pytest
+import numpy as np
+
+from scipy.io import loadmat
 
 from pydnslab.createfields import Fields
+
+FIELDS_PATH = os.path.join(os.path.dirname(__file__), 'fields')
 
 DEFAULT_CASE = dict(
     res=32,
@@ -12,9 +18,19 @@ DEFAULT_CASE = dict(
     u_f=0.4
 )
 
+def load_mat_file(name: str) -> np.ndarray:
+    # Load the MatLab file
+    mat_file = loadmat(os.path.join(FIELDS_PATH, name))
+    array = mat_file[list(mat_file)[-1]]
+    # Matlab arrays are stored in Fortran order, so we need to reorder
+    array = array.flatten(order='F').reshape(np.shape(array))
+    return array
+
+
 @pytest.fixture
 def fields() -> Fields:
     return Fields(DEFAULT_CASE)
+
 
 def test_num_gridpoints(fields: Fields) -> None:
     assert fields.N1 == 32
@@ -27,9 +43,12 @@ def test_steps(fields: Fields) -> None:
     assert fields.dy == pytest.approx(0.098174770424681)
     assert fields.dz == pytest.approx(0.062500000000000)
     
-    
+
 def test_enum_matrix(fields: Fields) -> None:
-    pass
+    A = load_mat_file('A.mat')
+    A -= 1 # Matlab is 1-indexed, Python is 0-indexed
+    np.testing.assert_array_equal(fields.A, A)
+    
 
 
 def test_xyz(fields: Fields) -> None:
