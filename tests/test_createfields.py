ec991f88eb59fa7d70cf82de3515e3947871dcae
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2022-10-15 22:06:23 +0200

    Test more indices

--- tests/test_createfields.py
+++ tests/test_createfields.py
@@ -69,6 +69,7 @@ def test_cell_size(fields: Fields) -> None:
     np.testing.assert_array_almost_equal(fields.FY, FY)
     np.testing.assert_array_almost_equal(fields.FZ[:,:,1], FZ[:,:,1])
 
+
 def test_XYZ(fields: Fields) -> None:
     X = load_mat_file('X_.mat')
     Y = load_mat_file('Y_.mat')
@@ -77,20 +78,58 @@ def test_XYZ(fields: Fields) -> None:
     np.testing.assert_almost_equal(fields.Y, Y)
     np.testing.assert_almost_equal(fields.Z[:,:,1], Z[:,:,1])
 
+
 def test_inx(fields: Fields) -> None:
     inx = load_mat_file('inx.mat')
     inx = inx - 1
     np.testing.assert_almost_equal(fields.inx, inx)
+
 
 def test_iny(fields: Fields) -> None:
     iny = load_mat_file('iny.mat')
     iny = iny - 1
     np.testing.assert_almost_equal(fields.iny, iny)
 
+
 def test_inz(fields: Fields) -> None:
     inz = load_mat_file('inz.mat')
     inz = inz - 1
     np.testing.assert_almost_equal(fields.inz, inz)
+
+
+def test_north(fields: Fields) -> None:
+    north = load_mat_file('north.mat')
+    north = north - 1
+    np.testing.assert_almost_equal(fields.north, north)
+
+def test_south(fields: Fields) -> None:
+    south = load_mat_file('south.mat')
+    south = south - 1
+    np.testing.assert_almost_equal(fields.south, south)
+
+
+def test_east(fields: Fields) -> None:
+    east = load_mat_file('east.mat')
+    east = east - 1
+    np.testing.assert_almost_equal(fields.east, east)
+
+
+def test_west(fields: Fields) -> None:
+    west = load_mat_file('west.mat')
+    west = west - 1
+    np.testing.assert_almost_equal(fields.west, west)
+
+
+def test_air(fields: Fields) -> None:
+    air = load_mat_file('air.mat')
+    air = air - 1
+    np.testing.assert_almost_equal(fields.air, air)
+
+
+def test_ground(fields: Fields) -> None:
+    ground = load_mat_file('ground.mat')
+    ground = ground - 1
+    np.testing.assert_almost_equal(fields.ground, ground)
 
 
 def test_UVW(fields: Fields) -> None:
