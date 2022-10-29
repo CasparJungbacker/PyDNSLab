3f0acdf548052097011eeac14ae2a5ce78f78f35
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2022-10-29 11:14:48 +0200

    Delete unused files

--- pydnslab/preconditioners.py
+++
@@ -1,8 +0,0 @@
-import scipy.sparse as sps
-
-def preconditioners(solver: str, M: sps.dok_matrix) -> sps.dok_matrix:
-    
-    if solver == 'sbigc':
-        A_prc = sps.linalg.spilu(M)
-    elif solver == 'spcg':
-        
