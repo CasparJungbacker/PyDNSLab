b2c54d9e639c6e934e5c1481a5807a01c5387cbd
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2022-11-06 21:14:44 +0100

    Add abstract solver class

---
+++ pydnslab/basesolver.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from abc import ABC, abstractmethod
+
+from pydnslab.createfields import Fields
+from pydnslab.base_operators import Operators
+
+
+class Solver(ABC):
+    @staticmethod
+    @abstractmethod
+    def projections(fields: Fields, operators: Operators) -> None:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def adjust_timestep(fields: Fields, dt: float, case: str) -> float:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def timestep(
+        fields: Fields,
+        operators: Operators,
+        s: int,
+        a: np.ndarray,
+        b: np.ndarray,
+        c: np.ndarray,
+        dt: float,
+        nu: float,
+        gx: float,
+        gy: float,
+        gz: float,
+    ) -> None:
+        pass
