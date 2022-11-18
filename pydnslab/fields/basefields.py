6d2e6bc8a3b153fe72a85a492a9db3f70ffa476a
CasparJungbacker <caspar.jungbacker@outlook.com>
2022-11-18 20:34:05 +0100

    Add `Fields` ABC

---
+++ pydnslab/fields/basefields.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Fields(ABC):
+    @abstractmethod
+    def __init__(self, dim: tuple, runmode: int, u_nom: float, u_f: float) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, dp: Any, du: Any, dv: Any, dw: Any):
+        pass
