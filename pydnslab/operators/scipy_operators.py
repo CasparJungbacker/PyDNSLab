Fix first order derivatives

pydnslab/operators/scipy_operators.py
@@ -90,40 +90,30 @@ class ScipyOperators:
 
         elif index == 3:
             data_1 = (1 / FZ0) * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0))
-            data_2 = np.zeros_like(data_1)
-            data_3 = np.zeros_like(data_1)
-            cols_2 = np.zeros_like(data_1)
-            cols_3 = np.zeros_like(data_1)
-            rows_2 = np.zeros_like(data_1)
-            rows_3 = np.zeros_like(data_1)
 
             mask = grid.AG[grid.A0.flatten()] >= 0
 
-            data_2[mask] = (-1 / FZ0 * FZ0 / (FZ0 + FZG))[mask]
-            data_2[~mask] = (
+            data_2 = (-1 / FZ0 * FZ0 / (FZ0 + FZG))[mask]
+            data_1[~mask] = (
                 1 / FZ0 * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0) + FZ0 / (FZG + FZ0))
             )[~mask]
 
-            cols_2[mask] = grid.AG[grid.A0.flatten()[mask]]
-            cols_2[~mask] = grid.A0.flatten()[~mask]
+            cols_2 = grid.AG[grid.A0.flatten()[mask]]
 
-            rows_2[mask] = grid.A0.flatten()[mask]
-            rows_2[~mask] = grid.A0.flatten()[~mask]
+            rows_2 = grid.A0.flatten()[mask]
 
             mask = grid.AA[grid.A0.flatten()] <= int(
                 grid.N1 * grid.N2 * (grid.N3 - 2) - 1
             )
 
-            data_3[mask] = (1 / FZ0 * FZ0 / (FZ0 + FZA))[mask]
-            data_3[~mask] = (
+            data_3 = (1 / FZ0 * FZ0 / (FZ0 + FZA))[mask]
+            data_1[~mask] = (
                 1 / FZ0 * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0) - FZ0 / (FZA + FZ0))
             )[~mask]
 
-            cols_3[mask] = grid.AA[grid.A0.flatten()[mask]]
-            cols_3[~mask] = grid.A0.flatten()[~mask]
+            cols_3 = grid.AA[grid.A0.flatten()[mask]]
 
-            rows_3[mask] = grid.A0.flatten()[mask]
-            rows_3[~mask] = grid.A0.flatten()[~mask]
+            rows_3 = grid.A0.flatten()[mask]
 
             data = np.concatenate((data_1, data_2, data_3))
             rows = np.concatenate((grid.A0.flatten(), rows_2, rows_3))
@@ -283,30 +273,22 @@ class ScipyOperators:
             data_1 = -2 / (FZ0 * (FZ0 + FZA)) - 2 / (FZ0 * (FZ0 + FZG))
             data_2 = np.zeros_like(data_1)
             data_3 = np.zeros_like(data_1)
-            cols_2 = np.zeros_like(data_1)
-            cols_3 = np.zeros_like(data_1)
 
             mask = grid.AG[grid.A0.flatten()] >= 0
 
             data_2[mask] = (2 / (FZ0 * (FZ0 + FZG)))[mask]
-            data_2[~mask] = (-2 / ((FZ0 * (FZ0 + FZA))) - 4 / (FZ0 * (FZ0 + FZG)))[
-                ~mask
-            ]
+            data_1[~mask] = (-2 / (FZ0 * (FZ0 + FZA)) - 4 / (FZ0 * (FZ0 + FZG)))[~mask]
 
-            cols_2[mask] = grid.AG[mask]
-            cols_2[~mask] = grid.A0.flatten()[~mask]
+            cols_2 = grid.AG[mask]
 
             mask = grid.AA[grid.A0.flatten()] <= int(
                 grid.N1 * grid.N2 * (grid.N3 - 2) - 1
             )
 
             data_3[mask] = (2 / (FZ0 * (FZ0 + FZA)))[mask]
-            data_3[~mask] = (-4 / ((FZ0 * (FZ0 + FZA))) - 2 / (FZ0 * (FZ0 + FZG)))[
-                ~mask
-            ]
+            data_1[~mask] = (-4 / (FZ0 * (FZ0 + FZA)) - 2 / (FZ0 * (FZ0 + FZG)))[~mask]
 
-            cols_3[mask] = grid.AA[mask]
-            cols_3[~mask] = grid.A0.flatten()[~mask]
+            cols_3 = grid.AA[mask]
 
             data = np.concatenate((data_1, data_2, data_3))
             rows = np.tile(grid.A0.flatten(), 3)

tests/scipy/test_operators.py
@@ -29,7 +29,7 @@ def test_differentiate_1(grid: Grid, ind: int, mat: str) -> None:
     i, j, v = sp.sparse.find(M)
     np.testing.assert_array_equal(rows, i)
     np.testing.assert_array_equal(cols, j)
-    np.testing.assert_array_equal(data, v)
+    np.testing.assert_almost_equal(data, v)
 
 
 def test_differentiate_1p():