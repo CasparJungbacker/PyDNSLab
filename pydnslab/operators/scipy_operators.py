Fix pressure derivatives + add tests

pydnslab/operators/scipy_operators.py
@@ -179,35 +179,31 @@ class ScipyOperators:
 
         elif index == 3:
             data_1 = (1 / FZ0) * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0))
-            data_2 = np.zeros_like(data_1)
-            data_3 = np.zeros_like(data_1)
-            cols_2 = np.zeros_like(data_1)
-            cols_3 = np.zeros_like(data_1)
 
             mask = grid.AG[grid.A0.flatten()] >= 0
 
-            data_2[mask] = (-1 / FZ0 * (FZ0 / (FZ0 + FZG)))[mask]
-            data_2[~mask] = (
-                1 / FZ0 * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0) + FZ0 / (FZG + FZ0))
+            data_2 = (-1 / FZ0 * (FZ0 / (FZ0 + FZG)))[mask]
+            data_1[~mask] = (
+                1 / FZ0 * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0) - FZ0 / (FZG + FZ0))
             )[~mask]
 
-            cols_2[mask] = grid.AG[mask]
-            cols_2[~mask] = grid.A0.flatten()[~mask]
+            cols_2 = grid.AG[mask]
+            rows_2 = grid.A0.flatten()[mask]
 
             mask = grid.AA[grid.A0.flatten()] <= int(
                 grid.N1 * grid.N2 * (grid.N3 - 2) - 1
             )
 
-            data_3[mask] = (1 / FZ0 * FZ0 / (FZ0 + FZA))[mask]
-            data_3[~mask] = (
-                1 / FZ0 * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0) - FZ0 / (FZA + FZ0))
+            data_3 = (1 / FZ0 * FZ0 / (FZ0 + FZA))[mask]
+            data_1[~mask] = (
+                1 / FZ0 * (FZA / (FZ0 + FZA) - FZG / (FZG + FZ0) + FZ0 / (FZA + FZ0))
             )[~mask]
 
-            cols_3[mask] = grid.AA[mask]
-            cols_3[~mask] = grid.A0.flatten()[~mask]
+            cols_3 = grid.AA[mask]
+            rows_3 = grid.A0.flatten()[mask]
 
             data = np.concatenate((data_1, data_2, data_3))
-            rows = np.tile(grid.A0.flatten(), 3)
+            rows = np.concatenate((grid.A0.flatten(), rows_2, rows_3))
             cols = np.concatenate((grid.A0.flatten(), cols_2, cols_3))
 
         else:

tests/scipy/test_operators.py
@@ -32,8 +32,14 @@ def test_differentiate_1(grid: Grid, ind: int, mat: str) -> None:
     np.testing.assert_almost_equal(data, v)
 
 
-def test_differentiate_1p():
-    pass
+@pytest.mark.parametrize("ind, mat", [(2, "Dxp.txt"), (1, "Dyp.txt"), (3, "Dzp.txt")])
+def test_differentiate_1p(grid: Grid, ind: int, mat: str) -> None:
+    rows, cols, data = load_operators(mat)
+    M = ScipyOperators.differentiate_1p(grid, ind)
+    i, j, v = sp.sparse.find(M)
+    np.testing.assert_array_equal(rows, i)
+    np.testing.assert_array_equal(cols, j)
+    np.testing.assert_almost_equal(data, v)
 
 
 def test_differentiate_2():