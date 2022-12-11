04a1078a740762a10887666e125332294305ef73
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2022-12-11 20:53:51 +0100

    Pre-allocate arrays for statistics

--- pydnslab/statistics.py
+++ pydnslab/statistics.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 from pydnslab.grid import Grid
 from pydnslab.fields.basefields import Fields
@@ -25,42 +25,46 @@ class Statistics:
             self.interval, case["nsteps"] + self.interval, self.interval, dtype=int
         )
 
+        stat_dim = (len(samples), len(self._z1))
+
+        self.row = 0
+
         # Declare statistics
         # TODO: pre-allocate these arrays, and don't use np.vstack later on
-        self.uplumean = None
-        self.vplumean = None
-        self.wplumean = None
-        self.yplumean = None
-        self.ut_mean = None
 
-        self.uutmean = None
-        self.uuplumean = None
-        self.vvplumean = None
-        self.wwplumean = None
-        self.yyplumean = None
-        self.taveuplumean = None
-        self.tavevplumean = None
-        self.tavewplumean = None
-        self.taveyplumean = None
+        self.uutmean = np.zeros(stat_dim)
+        self.uuplumean = np.zeros(stat_dim)
+        self.vvplumean = np.zeros(stat_dim)
+        self.wwplumean = np.zeros(stat_dim)
+        self.yyplumean = np.zeros(stat_dim)
+        self.taveuplumean = np.zeros(stat_dim)
+        self.tavevplumean = np.zeros(stat_dim)
+        self.tavewplumean = np.zeros(stat_dim)
+        self.taveyplumean = np.zeros(stat_dim)
 
-        self.uufluc = None
-        self.vvfluc = None
-        self.wwfluc = None
-        self.uuvvfluc = None
-        self.uuwwfluc = None
-        self.vvwwfluc = None
+        self.uufluc = np.zeros(stat_dim)
+        self.vvfluc = np.zeros(stat_dim)
+        self.wwfluc = np.zeros(stat_dim)
+        self.uuvvfluc = np.zeros(stat_dim)
+        self.uuwwfluc = np.zeros(stat_dim)
+        self.vvwwfluc = np.zeros(stat_dim)
 
-        self.uplurms = None
-        self.vplurms = None
-        self.wplurms = None
+        self.uplurms = np.zeros(stat_dim)
+        self.vplurms = np.zeros(stat_dim)
+        self.wplurms = np.zeros(stat_dim)
 
-        self.uupluvvplumean = None
-        self.uupluwwplumean = None
-        self.vvpluwwplumean = None
+        self.uupluvvplumean = np.zeros(stat_dim)
+        self.uupluwwplumean = np.zeros(stat_dim)
+        self.vvpluwwplumean = np.zeros(stat_dim)
 
     def update(self, grid: Grid, fields: Fields, i: int) -> None:
-        if i % self.interval == 0:
+
+        if i % self.interval == 0 and i > 0:
             self.update_fluctuations(grid, fields)
+            self.row += 1
+
+    def plot(self) -> None:
+        self._plot_fluctiations()
 
     def update_fluctuations(self, grid: Grid, fields: Fields) -> None:
         # Shear velocity at the bottom
@@ -75,121 +79,129 @@ class Statistics:
 
         ut2_mean = ut2.mean()
 
-        self.ut_mean = 0.5 * (ut1_mean + ut2_mean)
+        ut_mean = 0.5 * (ut1_mean + ut2_mean)
 
-        uplu1 = fields.U[self._z1] / self.ut_mean
+        uplu1 = fields.U[self._z1] / ut_mean
 
         uplu1_mean = uplu1.mean(axis=(1, 2))
 
-        uplu2 = fields.U[self._z2] / self.ut_mean
+        uplu2 = fields.U[self._z2] / ut_mean
         uplu2 = np.flip(uplu2, axis=0)
         uplu2_mean = uplu2.mean(axis=(1, 2))
 
-        vplu1 = fields.W[self._z1] / self.ut_mean
+        vplu1 = fields.W[self._z1] / ut_mean
 
         vplu1_mean = vplu1.mean(axis=(1, 2))
 
-        vplu2 = fields.W[self._z2] / self.ut_mean * -1
+        vplu2 = fields.W[self._z2] / ut_mean * -1
         vplu2 = np.flip(vplu2, axis=0)
         vplu2_mean = vplu2.mean(axis=(1, 2))
         vplu2_mean = np.flip(vplu2_mean, axis=0)
 
-        wplu1 = fields.V[self._z1] / self.ut_mean
+        wplu1 = fields.V[self._z1] / ut_mean
 
         wplu1_mean = wplu1.mean(axis=(1, 2))
 
-        wplu2 = fields.V[self._z2] / self.ut_mean
+        wplu2 = fields.V[self._z2] / ut_mean
         wplu2 = np.flip(wplu2, axis=0)
         wplu2_mean = wplu2.mean(axis=(1, 2))
 
-        yplu1_mean = grid.z[1 : int(grid.N3 / 2)] / self.nu * self.ut_mean
+        yplu1_mean = grid.z[1 : int(grid.N3 / 2)] / self.nu * ut_mean
 
-        yplu2 = (grid.height - grid.z[int(grid.N3 / 2) : -1]) / self.nu * self.ut_mean
+        yplu2 = (grid.height - grid.z[int(grid.N3 / 2) : -1]) / self.nu * ut_mean
         yplu2_mean = np.flip(yplu2, axis=0)
 
-        self.uplumean = 0.5 * (uplu1_mean + uplu2_mean)
-        self.vplumean = 0.5 * (vplu1_mean + vplu2_mean)
-        self.wplumean = 0.5 * (wplu1_mean + wplu2_mean)
-        self.yplumean = 0.5 * (yplu1_mean + yplu2_mean)
-        self.yplumean = self.yplumean.reshape(int(grid.N3 / 2 - 1))
+        uplumean = 0.5 * (uplu1_mean + uplu2_mean)
+        vplumean = 0.5 * (vplu1_mean + vplu2_mean)
+        wplumean = 0.5 * (wplu1_mean + wplu2_mean)
+        yplumean = 0.5 * (yplu1_mean + yplu2_mean)
+        yplumean = yplumean.reshape(int(grid.N3 / 2 - 1))
 
-        if self.uplumean is None:
-            self.uutmean = self.ut_mean
-            self.uuplumean = self.uplumean
-            self.vvplumean = self.vplumean
-            self.wwplumean = self.wplumean
-            self.yyplumean = self.yplumean
-            self.taveuplumean = self.uplumean
-            self.tavevplumean = self.vplumean
-            self.tavewplumean = self.wplumean
-            self.taveyplumean = self.yplumean
+        self.uutmean[self.row] = ut_mean
+        self.uuplumean[self.row] = uplumean
+        self.vvplumean[self.row] = vplumean
+        self.wwplumean[self.row] = wplumean
+        self.yyplumean[self.row] = yplumean
+
+        if self.row == 0:
+            self.taveuplumean[self.row] = uplumean
+            self.tavevplumean[self.row] = vplumean
+            self.tavewplumean[self.row] = wplumean
+            self.taveyplumean[self.row] = yplumean
         else:
-            self.uutmean = np.vstack((self.uutmean, self.ut_mean))
-            self.uuplumean = np.vstack((self.uuplumean, self.uplumean))
-            self.vvplumean = np.vstack((self.vvplumean, self.vplumean))
-            self.wwplumean = np.vstack((self.wwplumean, self.wplumean))
-            self.yyplumean = np.vstack((self.yyplumean, self.yplumean))
-            self.taveuplumean = np.mean(self.uuplumean, axis=0)
-            self.tavevplumean = np.mean(self.vvplumean, axis=0)
-            self.tavewplumean = np.mean(self.wwplumean, axis=0)
-            self.taveyplumean = np.mean(self.yyplumean, axis=0)
+            self.taveuplumean[self.row] = np.mean(
+                self.uuplumean[: self.row + 1], axis=0
+            )
+            self.tavevplumean[self.row] = np.mean(
+                self.vvplumean[: self.row + 1], axis=0
+            )
+            self.tavewplumean[self.row] = np.mean(
+                self.wwplumean[: self.row + 1], axis=0
+            )
+            self.taveyplumean[self.row] = np.mean(
+                self.yyplumean[: self.row + 1], axis=0
+            )
 
-        matuplumean = np.zeros((len(self.taveuplumean), grid.N1, grid.N2))
-        matvplumean = np.zeros((len(self.tavevplumean), grid.N1, grid.N2))
-        matwplumean = np.zeros((len(self.tavewplumean), grid.N1, grid.N2))
+        matuplumean = np.zeros((len(self.taveuplumean[0]), grid.N1, grid.N2))
+        matvplumean = np.zeros((len(self.tavevplumean[0]), grid.N1, grid.N2))
+        matwplumean = np.zeros((len(self.tavewplumean[0]), grid.N1, grid.N2))
 
-        for nd in grid.N1:
-            for md in grid.N2:
-                matuplumean[:, nd, md] = self.taveuplumean
-                matvplumean[:, nd, md] = self.tavevplumean
-                matwplumean[:, nd, md] = self.tavewplumean
+        for nd in range(grid.N1):
+            for md in range(grid.N2):
+                matuplumean[:, nd, md] = self.taveuplumean[self.row]
+                matvplumean[:, nd, md] = self.tavevplumean[self.row]
+                matwplumean[:, nd, md] = self.tavewplumean[self.row]
 
-        uflucsq = self.calc_sq_fluc(uplu1, uplu2, matuplumean)
-        vflucsq = self.calc_sq_fluc(vplu1, vplu2, matvplumean)
-        wflucsq = self.calc_sq_fluc(wplu1, wplu2, matwplumean)
+        uflucsq = self._calc_sq_fluc(uplu1, uplu2, matuplumean)
+        vflucsq = self._calc_sq_fluc(vplu1, vplu2, matvplumean)
+        wflucsq = self._calc_sq_fluc(wplu1, wplu2, matwplumean)
 
-        uvfluc = self.calc_cross_sq_fluc(
+        uvfluc = self._calc_cross_sq_fluc(
             uplu1, vplu1, uplu2, vplu2, matuplumean, matvplumean
         )
-        uwfluc = self.calc_cross_sq_fluc(
+        uwfluc = self._calc_cross_sq_fluc(
             uplu1, wplu1, uplu2, wplu2, matuplumean, matwplumean
         )
-        vwfluc = self.calc_cross_sq_fluc(
+        vwfluc = self._calc_cross_sq_fluc(
             vplu1, wplu1, vplu2, wplu2, matvplumean, matwplumean
         )
 
-        if self.uufluc is None:
-            self.uufluc = uflucsq
-            self.vvfluc = vflucsq
-            self.wwfluc = wflucsq
-            self.uplurms = np.sqrt(uflucsq)
-            self.vplurms = np.sqrt(vflucsq)
-            self.wplurms = np.sqrt(wflucsq)
+        self.uufluc[self.row] = uflucsq
+        self.vvfluc[self.row] = vflucsq
+        self.wwfluc[self.row] = wflucsq
+        self.uuvvfluc[self.row] = uvfluc
+        self.uuwwfluc[self.row] = uwfluc
+        self.vvwwfluc[self.row] = vwfluc
 
-            self.uuvvfluc = uvfluc
-            self.uuwwfluc = uwfluc
-            self.vvwwfluc = vwfluc
-            self.uupluvvplumean = uvfluc
-            self.uupluwwplumean = uwfluc
-            self.vvpluwwplumean = vwfluc
+        if self.row == 0:
+            self.uplurms[self.row] = np.sqrt(uflucsq)
+            self.vplurms[self.row] = np.sqrt(vflucsq)
+            self.wplurms[self.row] = np.sqrt(wflucsq)
+            self.uupluvvplumean[self.row] = uvfluc
+            self.uupluwwplumean[self.row] = uwfluc
+            self.vvpluwwplumean[self.row] = vwfluc
         else:
-            self.uufluc = np.vstack((self.uufluc, uflucsq))
-            self.vvfluc = np.vstack((self.vvfluc, vflucsq))
-            self.wwfluc = np.vstack((self.wwfluc, wflucsq))
-            self.uplurms = self.calc_plurms(self.uufluc)
-            self.vplurms = self.calc_plurms(self.vvfluc)
-            self.wplurms = self.calc_plurms(self.wwfluc)
-
-            self.uuvvfluc = np.vstack((self.uuvvfluc, uvfluc))
-            self.uuwwfluc = np.vstack((self.uuwwfluc, uwfluc))
-            self.vvwwfluc = np.vstack((self.vvwwfluc, vwfluc))
-
-            self.uupluvvplumean = np.mean(self.uuvvfluc, axis=0)
-            self.uupluwwplumean = np.mean(self.uuwwfluc, axis=0)
-            self.vvpluwwplumean = np.mean(self.vvwwfluc, axis=0)
+            self.uplurms[self.row] = np.square(
+                np.mean(self.uufluc[: self.row + 1], axis=0)
+            )
+            self.vplurms[self.row] = np.square(
+                np.mean(self.vvfluc[: self.row + 1], axis=0)
+            )
+            self.wplurms[self.row] = np.square(
+                np.mean(self.wwfluc[: self.row + 1], axis=0)
+            )
+            self.uupluvvplumean[self.row] = np.mean(
+                self.uuvvfluc[: self.row + 1], axis=0
+            )
+            self.uupluwwplumean[self.row] = np.mean(
+                self.uuwwfluc[: self.row + 1], axis=0
+            )
+            self.vvpluwwplumean[self.row] = np.mean(
+                self.vvwwfluc[: self.row + 1], axis=0
+            )
 
     @staticmethod
-    def calc_sq_fluc(
+    def _calc_sq_fluc(
         plu1: np.ndarray, plu2: np.ndarray, matplumean: np.ndarray
     ) -> np.ndarray:
         fluc1 = plu1 - matplumean
@@ -201,7 +213,7 @@ class Statistics:
         return flucsq
 
     @staticmethod
-    def calc_cross_sq_fluc(
+    def _calc_cross_sq_fluc(
         plu1a: np.ndarray,
         plu1b: np.ndarray,
         plu2a: np.ndarray,
@@ -215,8 +227,13 @@ class Statistics:
         fluc = np.mean(fluc, axis=(1, 2))
         return fluc
 
-    @staticmethod
-    def calc_plurms(fluc):
-        plurms = np.mean(fluc, axis=0)
-        plurms = np.square(plurms)
-        return plurms
+    def _plot_fluctiations(self) -> None:
+        fig, ax = plt.subplots(1, 3)
+
+        ax[0].semilogx(self.yyplumean[-2], self.uuplumean[-2])
+
+        ax[1].plot(self.yyplumean[-2], self.uplurms[-2])
+
+        ax[2].plot(self.yyplumean[-2], self.uupluvvplumean[-2])
+
+        fig.show()
